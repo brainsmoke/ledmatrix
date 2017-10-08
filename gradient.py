9888bcf39c3cf4c1fb64cf154ad6113c738e09a1
Erik Bosman <erik@minemu.org>
2017-10-08 19:59:32 +0200

    clarify gradient program

--- gradient.py
+++ gradient.py
@@ -1,9 +1,16 @@
 import sys,time
 
+
+def next_frame(i, t):
+	return [ 255-((x+i)%120)*2 for y in range(8) for x in range(120) ]
+
+
 i=0
+t0 = time.time()
+
 while True:
 	i+=1
 	i%=120
-	sys.stdout.write("".join(chr(127-(x+i)%120) for x in range(120))*8+"\x80")
+	sys.stdout.write(''.join(chr((x/2)&0x7f) for x in next_frame(i, time.time()-t0))+"\x80")
 	sys.stdout.flush()
 	time.sleep(.005)
